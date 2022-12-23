2a758edb3f86f8f4343ddcae7a86d529bec71724
Vidhi1793 <112926154+Vidhi1793@users.noreply.github.com>
2022-12-23 09:48:48 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -2,29 +2,18 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
 
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
-    static getAllTodos() {
-      return this.findAll({ order: [["id", "ASC"]] });
-    }
-
     markAsCompleted() {
       return this.update({ completed: true });
     }
-
-    delete() {
-      return this.destroy();
+    static getTodos() {
+      return this.findAll({ order: [["id", "ASC"]] });
     }
   }
   Todo.init(
